c7624f9ad7de6bbea841cc06f796b4e26f2c6d29
Mikhail Mahamood <mikhailmahamood@gmail.com>
2025-12-09 14:34:10 +0800

    all fltk stuff are on heap now, since fltk recursively deletes for me, also tested out task boxes

--- FLTK_wireframe.cpp
+++ FLTK_wireframe.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <memory>
+#include <vector>
+
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Button.H>
@@ -12,57 +15,71 @@
 const int WINDOW_WIDTH{640};
 const int WINDOW_HEIGHT{480};
 
+Fl_Group* newTaskBox(std::string & taskTitle)
+{
+	Fl_Group* row = new Fl_Group(0, 0, 480, 40);
+	row->begin();
+
+	Fl_Box* item1 = new Fl_Box(FL_FLAT_BOX, 5, 5, 430, 30, taskTitle.c_str()); 
+	item1->labelsize(18);
+	item1->box(FL_UP_BOX);
+	item1->color(FL_WHITE);
+	// item1->copy_label(taskTitle.c_str());
+
+	Fl_Check_Button* checkButton = new Fl_Check_Button(445, 10, 20, 20);
+
+	row->end();
+	// row->resizable(item1);
+	return row; 
+}
+
+static std::vector<std::string> taskTitlesTest = {"buy chicken", "buy fish", "buy coffee"};
+
 int main()
 {
-	std::cout << "hello world!" << '\n';
-	Fl_Window window = Fl_Window(WINDOW_WIDTH, WINDOW_HEIGHT, "HELLO FLTK!");
 
-	Fl_Pack buttonsPack = Fl_Pack(510, 10, 110, 460);
-	buttonsPack.type(Fl_Pack::VERTICAL);
-	buttonsPack.spacing(10);
-	buttonsPack.begin();
+	std::cout << "hello world!" << '\n';
+	Fl_Window* window = new Fl_Window(WINDOW_WIDTH, WINDOW_HEIGHT, "HELLO FLTK!");
+
+	Fl_Pack* buttonsPack = new Fl_Pack(510, 10, 110, 460);
+	buttonsPack->type(Fl_Pack::VERTICAL);
+	buttonsPack->spacing(10);
+	buttonsPack->begin();
 
 	// buttons on the side 
-	Fl_Button addButton = Fl_Button(0, 0, 80, 30, "add");
-	Fl_Button deleteButton = Fl_Button(0, 0, 80,30, "delete");	
-	Fl_Button markButton = Fl_Button(0, 0, 80, 30, "mark");
-	Fl_Button sortButton = Fl_Button(0, 0, 80, 30, "sort");
-	buttonsPack.end();
+	new Fl_Button(0, 0, 80, 30, "add");
+	new Fl_Button(0, 0, 80,30, "delete");	
+	new Fl_Button(0, 0, 80, 30, "mark");
+	new Fl_Button(0, 0, 80, 30, "sort");
+
+	buttonsPack->end();
 
 	// Fl_Scroll(int x, int y, int w, int h, const* char label = 0)
 	// this gives a scrollable box
-	Fl_Scroll scroll = Fl_Scroll(10, 10, 480, 460);	
-	scroll.box(FL_BORDER_BOX);
-	scroll.type(Fl_Scroll::VERTICAL_ALWAYS);
-	scroll.begin();
+	Fl_Scroll* scroll = new Fl_Scroll(10, 10, 480, 460);	
+	scroll->box(FL_BORDER_BOX);
+	scroll->type(Fl_Scroll::VERTICAL_ALWAYS);
+	scroll->begin();
 		
 	// this is in scrollable box and packs boxes nicely
-	Fl_Pack pack1 = Fl_Pack(0, 0, 480, 460);
-	pack1.type(Fl_Pack::VERTICAL);
-	pack1.spacing(1);
-	pack1.begin();
+	Fl_Pack* pack1 = new Fl_Pack(0, 0, 480, 460);
+	pack1->type(Fl_Pack::VERTICAL);
+	pack1->spacing(1);
+	pack1->begin();
 
 	// these are boxes that are packed nicely
-	Fl_Group row1 = Fl_Group(0, 0, 480, 40);
-	row1.begin();
-	Fl_Box item1 = Fl_Box(FL_FLAT_BOX, 0, 0, 480, 40, "buy milk"); 
-	item1.labelsize(18);
-	item1.box(FL_UP_BOX);
-	item1.color(FL_WHITE);
+	// each group is a box of the task, and a check box
+	std::vector<Fl_Group*> rows;
+	for (std::string & s : taskTitlesTest)
+	{
+		std::cout << s << '\n';
+		rows.push_back(newTaskBox(s));
+	}
+	pack1->end();
+	// pack1->resizable(nullptr);
+	scroll->end();
 
-	Fl_Check_Button checkButton(440, 10, 20, 20);
-	row1.end();
-
-	// this is task 2
-	Fl_Box item2 = Fl_Box(FL_FLAT_BOX, 0, 0, 480, 40, "buy food"); 
-	item2.labelsize(18);
-	item2.box(FL_UP_BOX);
-	item2.color(FL_GREEN);
-
-	pack1.end();
-	scroll.end();
-
-	window.end();
-	window.show();
+	window->end();
+	window->show();
 	return Fl::run();
 }
